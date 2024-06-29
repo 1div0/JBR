JetBrains/JetBrainsRuntime

JBR-4487 enable Linux 32 builds

jb/project/tools/linux/scripts/mkimages_x86.sh
@@ -1,80 +1,135 @@
 #!/bin/bash -x
 
 # The following parameters must be specified:
-#   JBSDK_VERSION    - specifies the current version of OpenJDK e.g. 11_0_6
-#   JDK_BUILD_NUMBER - specifies the number of OpenJDK build or the value of --with-version-build argument to configure
-#   build_number     - specifies the number of JetBrainsRuntime build
+#   build_number - specifies the number of JetBrainsRuntime build
+#   bundle_type  - specifies bundle to be built;possible values:
+#               <empty> or nomod - the release bundles without any additional modules (jcef)
+#               jcef - the release bundles with jcef
+#               fd - the fastdebug bundles which also include the jcef module
 #
-# jbrsdk-${JBSDK_VERSION}-osx-x64-b${build_number}.tar.gz
-# jbr-${JBSDK_VERSION}-osx-x64-b${build_number}.tar.gz
-#
-# $ ./java --version
-# openjdk 11.0.6 2020-01-14
-# OpenJDK Runtime Environment (build 11.0.6+${JDK_BUILD_NUMBER}-b${build_number})
-# OpenJDK 64-Bit Server VM (build 11.0.6+${JDK_BUILD_NUMBER}-b${build_number}, mixed mode)
-#
-
-JBSDK_VERSION=$1
-JDK_BUILD_NUMBER=$2
-build_number=$3
-
-JBSDK_VERSION_WITH_DOTS=$(echo $JBSDK_VERSION | sed 's/_/\./g')
 
 source jb/project/tools/common/scripts/common.sh
 
-JBRSDK_BASE_NAME=jbrsdk-${JBSDK_VERSION}
+function do_configure {
+  linux32 bash configure \
+    $WITH_DEBUG_LEVEL \
+    --with-vendor-name="$VENDOR_NAME" \
+    --with-vendor-version-string="$VENDOR_VERSION_STRING" \
+    --with-jvm-features=shenandoahgc \
+    --with-version-pre= \
+    --with-version-build="$JDK_BUILD_NUMBER" \
+    --with-version-opt=b"$build_number" \
+    --with-boot-jdk="$BOOT_JDK" \
+    --enable-cds=yes \
+    $REPRODUCIBLE_BUILD_OPTS \
+    $WITH_ZIPPED_NATIVE_DEBUG_SYMBOLS \
+    || do_exit $?
+}
 
-[ -z "$bundle_type" ] && (git apply -p0 < jb/project/tools/patches/exclude_jcef_module.patch || exit $?)
+function is_musl {
+  libc=$(ldd /bin/ls | grep 'musl' | head -1 | cut -d ' ' -f1)
+  if [ -z $libc ]; then
+    # This is not Musl, return 1 == false
+    return 1
+  fi
+  return 0
+}
 
-linux32 bash configure \
-  --with-debug-level=release \
-  --with-vendor-name="${VENDOR_NAME}" \
-  --with-vendor-version-string="${VENDOR_VERSION_STRING}" \
-  --with-version-pre= \
-  --with-version-build=$JDK_BUILD_NUMBER \
-  --with-version-opt=b${build_number} \
-  --with-boot-jdk=${BOOT_JDK} \
-  --enable-cds=yes || exit $?
-make clean CONF=linux-x86-server-release || exit $?
-make images CONF=linux-x86-server-release test-image || exit $?
+function create_image_bundle {
+  __bundle_name=$1
+  __arch_name=$2
+  __modules_path=$3
+  __modules=$4
 
-JBSDK=${JBRSDK_BASE_NAME}-linux-x86-b${build_number}
-BASE_DIR=build/linux-x86-server-release/images
-JSDK=${BASE_DIR}/jdk
+  libc_type_suffix=''
+
+  if is_musl; then libc_type_suffix='musl-' ; fi
+
+  [ "$bundle_type" == "fd" ] && [ "$__arch_name" == "$JBRSDK_BUNDLE" ] && __bundle_name=$__arch_name && fastdebug_infix="fastdebug-"
+  JBR=${__bundle_name}-${JBSDK_VERSION}-linux-${libc_type_suffix}x64-${fastdebug_infix}b${build_number}
+
+  echo Running jlink....
+  [ -d "$IMAGES_DIR"/"$__arch_name" ] && rm -rf "${IMAGES_DIR:?}"/"$__arch_name"
+  $JSDK/bin/jlink \
+    --module-path "$__modules_path" --no-man-pages --compress=2 \
+    --add-modules "$__modules" --output "$IMAGES_DIR"/"$__arch_name"
+
+  grep -v "^JAVA_VERSION" "$JSDK"/release | grep -v "^MODULES" >> "$IMAGES_DIR"/"$__arch_name"/release
+  if [ "$__arch_name" == "$JBRSDK_BUNDLE" ]; then
+    sed 's/JBR/JBRSDK/g' "$IMAGES_DIR"/"$__arch_name"/release > release
+    mv release "$IMAGES_DIR"/"$__arch_name"/release
+    cp $IMAGES_DIR/jdk/lib/src.zip "$IMAGES_DIR"/"$__arch_name"/lib
+    copy_jmods "$__modules" "$__modules_path" "$IMAGES_DIR"/"$__arch_name"/jmods
+    zip_native_debug_symbols $IMAGES_DIR/jdk "${JBR}_diz"
+  fi
+
+  # jmod does not preserve file permissions (JDK-8173610)
+  [ -f "$IMAGES_DIR"/"$__arch_name"/lib/jcef_helper ] && chmod a+x "$IMAGES_DIR"/"$__arch_name"/lib/jcef_helper
+
+  echo Creating "$JBR".tar.gz ...
+
+  (cd "$IMAGES_DIR" &&
+    find "$__arch_name" -print0 | LC_ALL=C sort -z | \
+    tar $REPRODUCIBLE_TAR_OPTS \
+      --no-recursion --null -T - -cf "$JBR".tar) || do_exit $?
+  mv "$IMAGES_DIR"/"$JBR".tar ./"$JBR".tar
+  [ -f "$JBR".tar.gz ] && rm "$JBR.tar.gz"
+  touch -c -d "@$SOURCE_DATE_EPOCH" "$JBR".tar
+  gzip "$JBR".tar || do_exit $?
+  rm -rf "${IMAGES_DIR:?}"/"$__arch_name"
+}
+
+WITH_DEBUG_LEVEL="--with-debug-level=release"
+RELEASE_NAME=linux-x86-server-release
+
+case "$bundle_type" in
+  "jcef")
+    echo "not implemented" && do_exit 1
+    ;;
+  "nomod" | "")
+    bundle_type=""
+    ;;
+  "fd")
+    do_reset_changes=1
+    WITH_DEBUG_LEVEL="--with-debug-level=fastdebug"
+    RELEASE_NAME=linux-x86-server-fastdebug
+    ;;
+esac
+
+if [ -z "$INC_BUILD" ]; then
+  do_configure || do_exit $?
+  make clean CONF=$RELEASE_NAME || do_exit $?
+fi
+make images CONF=$RELEASE_NAME || do_exit $?
+
+IMAGES_DIR=build/$RELEASE_NAME/images
+JSDK=$IMAGES_DIR/jdk
+JSDK_MODS_DIR=$IMAGES_DIR/jmods
 JBRSDK_BUNDLE=jbrsdk
 
 echo Fixing permissions
 chmod -R a+r $JSDK
 
-rm -rf $BASE_DIR/$JBRSDK_BUNDLE
-cp -r $JSDK $BASE_DIR/$JBRSDK_BUNDLE || exit $?
+if [ "$bundle_type" == "jcef" ] || [ "$bundle_type" == "fd" ]; then
+  jbr_name_postfix="_${bundle_type}"
+fi
 
-echo Creating $JBSDK.tar.gz ...
-sed 's/JBR/JBRSDK/g' ${BASE_DIR}/${JBRSDK_BUNDLE}/release > release
-mv release ${BASE_DIR}/${JBRSDK_BUNDLE}/release
+# create runtime image bundle
+modules=$(grep -v "jdk.internal.vm" jb/project/tools/common/modules.list | xargs | sed s/" "//g) || do_exit $?
+create_image_bundle "jbr${jbr_name_postfix}" "jbr" $JSDK_MODS_DIR "$modules" || do_exit $?
 
-tar -pcf $JBSDK.tar --exclude=*.debuginfo --exclude=demo --exclude=sample --exclude=man -C $BASE_DIR ${JBRSDK_BUNDLE} || exit $?
-gzip $JBSDK.tar || exit $?
+# create sdk image bundle
+modules=$(cat $JSDK/release | grep MODULES | sed s/MODULES=//g | sed s/' '/','/g | sed s/\"//g | sed s/\\n//g) || do_exit $?
+create_image_bundle "$JBRSDK_BUNDLE${jbr_name_postfix}" $JBRSDK_BUNDLE $JSDK_MODS_DIR "$modules" || do_exit $?
 
-JBR_BUNDLE=jbr
-JBR_BASE_NAME=jbr-$JBSDK_VERSION
-rm -rf $BASE_DIR/$JBR_BUNDLE
+if [ $do_maketest -eq 1 ]; then
+    JBRSDK_TEST=${JBRSDK_BUNDLE}-${JBSDK_VERSION}-linux-${libc_type_suffix}test-x86-b${build_number}
+    echo Creating "$JBRSDK_TEST" ...
+    [ $do_reset_changes -eq 1 ] && git checkout HEAD jb/project/tools/common/modules.list src/java.desktop/share/classes/module-info.java
+    make test-image CONF=$RELEASE_NAME || do_exit $?
+    tar -pcf "$JBRSDK_TEST".tar -C $IMAGES_DIR --exclude='test/jdk/demos' test || do_exit $?
+    [ -f "$JBRSDK_TEST.tar.gz" ] && rm "$JBRSDK_TEST.tar.gz"
+    gzip "$JBRSDK_TEST".tar || do_exit $?
+fi
 
-JBR=$JBR_BASE_NAME-linux-x86-b$build_number
-grep -v javafx jb/project/tools/common/modules.list | grep -v "jdk.internal.vm\|jdk.aot\|jcef" > modules.list.x86
-echo Running jlink....
-${JSDK}/bin/jlink \
-  --module-path ${JSDK}/jmods --no-man-pages --compress=2 \
-  --add-modules $(xargs < modules.list.x86 | sed s/" "//g | sed s/,$//g) --output ${BASE_DIR}/${JBR_BUNDLE} || exit $?
-
-echo Modifying release info ...
-grep -v \"^JAVA_VERSION\" ${JSDK}/release | grep -v \"^MODULES\" >> ${BASE_DIR}/${JBR_BUNDLE}/release
-
-echo Creating $JBR.tar.gz ...
-tar -pcf $JBR.tar -C $BASE_DIR $JBR_BUNDLE || exit $?
-gzip $JBR.tar || exit $?
-
-JBRSDK_TEST=$JBRSDK_BASE_NAME-linux-test-x86-b$build_number
-echo Creating $JBRSDK_TEST.tar.gz ...
-tar -pcf $JBRSDK_TEST.tar -C $BASE_DIR --exclude='test/jdk/demos' --exclude='test/hotspot/gtest' test || exit $?
-gzip $JBRSDK_TEST.tar || exit $?
+do_exit 0

1div0/JBR

jb/project/tools/linux/scripts/mkimages_x86.sh
@@ -1,81 +1,136 @@
 #!/bin/bash -x
 
 # The following parameters must be specified:
-#   JBSDK_VERSION    - specifies the current version of OpenJDK e.g. 11_0_6
-#   JDK_BUILD_NUMBER - specifies the number of OpenJDK build or the value of --with-version-build argument to configure
-#   build_number     - specifies the number of JetBrainsRuntime build
+#   build_number - specifies the number of JetBrainsRuntime build
+#   bundle_type  - specifies bundle to be built;possible values:
+#               <empty> or nomod - the release bundles without any additional modules (jcef)
+#               jcef - the release bundles with jcef
+#               fd - the fastdebug bundles which also include the jcef module
 #
-# jbrsdk-${JBSDK_VERSION}-osx-x64-b${build_number}.tar.gz
-# jbr-${JBSDK_VERSION}-osx-x64-b${build_number}.tar.gz
-#
-# $ ./java --version
-# openjdk 11.0.6 2020-01-14
-# OpenJDK Runtime Environment (build 11.0.6+${JDK_BUILD_NUMBER}-b${build_number})
-# OpenJDK 64-Bit Server VM (build 11.0.6+${JDK_BUILD_NUMBER}-b${build_number}, mixed mode)
-#
-
-JBSDK_VERSION=$1
-JDK_BUILD_NUMBER=$2
-build_number=$3
-
-JBSDK_VERSION_WITH_DOTS=$(echo $JBSDK_VERSION | sed 's/_/\./g')
 
 source jb/project/tools/common/scripts/common.sh
 
-JBRSDK_BASE_NAME=jbrsdk-${JBSDK_VERSION}
+function do_configure {
+  linux32 bash configure \
+    $WITH_DEBUG_LEVEL \
+    --with-vendor-name="$VENDOR_NAME" \
+    --with-vendor-version-string="$VENDOR_VERSION_STRING" \
+    --with-jvm-features=shenandoahgc \
+    --with-version-pre= \
+    --with-version-build="$JDK_BUILD_NUMBER" \
+    --with-version-opt=b"$build_number" \
+    --with-boot-jdk="$BOOT_JDK" \
+    $STATIC_CONF_ARGS \
+    --enable-cds=yes \
+    $REPRODUCIBLE_BUILD_OPTS \
+    $WITH_ZIPPED_NATIVE_DEBUG_SYMBOLS \
+    || do_exit $?
+}
 
-[ -z "$bundle_type" ] && (git apply -p0 < jb/project/tools/patches/exclude_jcef_module.patch || exit $?)
+function is_musl {
+  libc=$(ldd /bin/ls | grep 'musl' | head -1 | cut -d ' ' -f1)
+  if [ -z $libc ]; then
+    # This is not Musl, return 1 == false
+    return 1
+  fi
+  return 0
+}
 
-linux32 bash configure \
-  --with-debug-level=release \
-  --with-vendor-name="${VENDOR_NAME}" \
-  --with-vendor-version-string="${VENDOR_VERSION_STRING}" \
-  --with-version-pre= \
-  --with-version-build=$JDK_BUILD_NUMBER \
-  --with-version-opt=b${build_number} \
-  --with-boot-jdk=${BOOT_JDK} \
-  $STATIC_CONF_ARGS \
-  --enable-cds=yes || exit $?
-make clean CONF=linux-x86-server-release || exit $?
-make images CONF=linux-x86-server-release test-image || exit $?
+function create_image_bundle {
+  __bundle_name=$1
+  __arch_name=$2
+  __modules_path=$3
+  __modules=$4
 
-JBSDK=${JBRSDK_BASE_NAME}-linux-x86-b${build_number}
-BASE_DIR=build/linux-x86-server-release/images
-JSDK=${BASE_DIR}/jdk
+  libc_type_suffix=''
+
+  if is_musl; then libc_type_suffix='musl-' ; fi
+
+  [ "$bundle_type" == "fd" ] && [ "$__arch_name" == "$JBRSDK_BUNDLE" ] && __bundle_name=$__arch_name && fastdebug_infix="fastdebug-"
+  JBR=${__bundle_name}-${JBSDK_VERSION}-linux-${libc_type_suffix}x86-${fastdebug_infix}b${build_number}
+
+  echo Running jlink....
+  [ -d "$IMAGES_DIR"/"$__arch_name" ] && rm -rf "${IMAGES_DIR:?}"/"$__arch_name"
+  $JSDK/bin/jlink \
+    --module-path "$__modules_path" --no-man-pages --compress=2 \
+    --add-modules "$__modules" --output "$IMAGES_DIR"/"$__arch_name"
+
+  grep -v "^JAVA_VERSION" "$JSDK"/release | grep -v "^MODULES" >> "$IMAGES_DIR"/"$__arch_name"/release
+  if [ "$__arch_name" == "$JBRSDK_BUNDLE" ]; then
+    sed 's/JBR/JBRSDK/g' "$IMAGES_DIR"/"$__arch_name"/release > release
+    mv release "$IMAGES_DIR"/"$__arch_name"/release
+    cp $IMAGES_DIR/jdk/lib/src.zip "$IMAGES_DIR"/"$__arch_name"/lib
+    copy_jmods "$__modules" "$__modules_path" "$IMAGES_DIR"/"$__arch_name"/jmods
+    zip_native_debug_symbols $IMAGES_DIR/jdk "${JBR}_diz"
+  fi
+
+  # jmod does not preserve file permissions (JDK-8173610)
+  [ -f "$IMAGES_DIR"/"$__arch_name"/lib/jcef_helper ] && chmod a+x "$IMAGES_DIR"/"$__arch_name"/lib/jcef_helper
+
+  echo Creating "$JBR".tar.gz ...
+
+  (cd "$IMAGES_DIR" &&
+    find "$__arch_name" -print0 | LC_ALL=C sort -z | \
+    tar $REPRODUCIBLE_TAR_OPTS \
+      --no-recursion --null -T - -cf "$JBR".tar) || do_exit $?
+  mv "$IMAGES_DIR"/"$JBR".tar ./"$JBR".tar
+  [ -f "$JBR".tar.gz ] && rm "$JBR.tar.gz"
+  touch -c -d "@$SOURCE_DATE_EPOCH" "$JBR".tar
+  gzip "$JBR".tar || do_exit $?
+  rm -rf "${IMAGES_DIR:?}"/"$__arch_name"
+}
+
+WITH_DEBUG_LEVEL="--with-debug-level=release"
+RELEASE_NAME=linux-x86-server-release
+
+case "$bundle_type" in
+  "jcef")
+    echo "not implemented" && do_exit 1
+    ;;
+  "nomod" | "")
+    bundle_type=""
+    ;;
+  "fd")
+    do_reset_changes=1
+    WITH_DEBUG_LEVEL="--with-debug-level=fastdebug"
+    RELEASE_NAME=linux-x86-server-fastdebug
+    ;;
+esac
+
+if [ -z "$INC_BUILD" ]; then
+  do_configure || do_exit $?
+  make clean CONF=$RELEASE_NAME || do_exit $?
+fi
+make images CONF=$RELEASE_NAME || do_exit $?
+
+IMAGES_DIR=build/$RELEASE_NAME/images
+JSDK=$IMAGES_DIR/jdk
+JSDK_MODS_DIR=$IMAGES_DIR/jmods
 JBRSDK_BUNDLE=jbrsdk
 
 echo Fixing permissions
 chmod -R a+r $JSDK
 
-rm -rf $BASE_DIR/$JBRSDK_BUNDLE
-cp -r $JSDK $BASE_DIR/$JBRSDK_BUNDLE || exit $?
+if [ "$bundle_type" == "jcef" ] || [ "$bundle_type" == "fd" ]; then
+  jbr_name_postfix="_${bundle_type}"
+fi
 
-echo Creating $JBSDK.tar.gz ...
-sed 's/JBR/JBRSDK/g' ${BASE_DIR}/${JBRSDK_BUNDLE}/release > release
-mv release ${BASE_DIR}/${JBRSDK_BUNDLE}/release
+# create runtime image bundle
+modules=$(grep -v "jdk.internal.vm" jb/project/tools/common/modules.list | xargs | sed s/" "//g) || do_exit $?
+create_image_bundle "jbr${jbr_name_postfix}" "jbr" $JSDK_MODS_DIR "$modules" || do_exit $?
 
-tar -pcf $JBSDK.tar --exclude=*.debuginfo --exclude=demo --exclude=sample --exclude=man -C $BASE_DIR ${JBRSDK_BUNDLE} || exit $?
-gzip $JBSDK.tar || exit $?
+# create sdk image bundle
+modules=$(cat $JSDK/release | grep MODULES | sed s/MODULES=//g | sed s/' '/','/g | sed s/\"//g | sed s/\\n//g) || do_exit $?
+create_image_bundle "$JBRSDK_BUNDLE${jbr_name_postfix}" $JBRSDK_BUNDLE $JSDK_MODS_DIR "$modules" || do_exit $?
 
-JBR_BUNDLE=jbr
-JBR_BASE_NAME=jbr-$JBSDK_VERSION
-rm -rf $BASE_DIR/$JBR_BUNDLE
+if [ $do_maketest -eq 1 ]; then
+    JBRSDK_TEST=${JBRSDK_BUNDLE}-${JBSDK_VERSION}-linux-${libc_type_suffix}test-x86-b${build_number}
+    echo Creating "$JBRSDK_TEST" ...
+    [ $do_reset_changes -eq 1 ] && git checkout HEAD jb/project/tools/common/modules.list src/java.desktop/share/classes/module-info.java
+    make test-image CONF=$RELEASE_NAME || do_exit $?
+    tar -pcf "$JBRSDK_TEST".tar -C $IMAGES_DIR --exclude='test/jdk/demos' test || do_exit $?
+    [ -f "$JBRSDK_TEST.tar.gz" ] && rm "$JBRSDK_TEST.tar.gz"
+    gzip "$JBRSDK_TEST".tar || do_exit $?
+fi
 
-JBR=$JBR_BASE_NAME-linux-x86-b$build_number
-grep -v javafx jb/project/tools/common/modules.list | grep -v "jdk.internal.vm\|jdk.aot\|jcef" > modules.list.x86
-echo Running jlink....
-${JSDK}/bin/jlink \
-  --module-path ${JSDK}/jmods --no-man-pages --compress=2 \
-  --add-modules $(xargs < modules.list.x86 | sed s/" "//g | sed s/,$//g) --output ${BASE_DIR}/${JBR_BUNDLE} || exit $?
-
-echo Modifying release info ...
-grep -v \"^JAVA_VERSION\" ${JSDK}/release | grep -v \"^MODULES\" >> ${BASE_DIR}/${JBR_BUNDLE}/release
-
-echo Creating $JBR.tar.gz ...
-tar -pcf $JBR.tar -C $BASE_DIR $JBR_BUNDLE || exit $?
-gzip $JBR.tar || exit $?
-
-JBRSDK_TEST=$JBRSDK_BASE_NAME-linux-test-x86-b$build_number
-echo Creating $JBRSDK_TEST.tar.gz ...
-tar -pcf $JBRSDK_TEST.tar -C $BASE_DIR --exclude='test/jdk/demos' --exclude='test/hotspot/gtest' test || exit $?
-gzip $JBRSDK_TEST.tar || exit $?
+do_exit 0