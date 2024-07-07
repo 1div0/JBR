JetBrains/JetBrainsRuntime

JBR-2736 provide writeObjects implementation for copying files/folders from Project Explorer to the Finder

src/java.desktop/macosx/native/libawt_lwawt/awt/CClipboard.m
@@ -176,6 +176,48 @@ JNF_COCOA_EXIT(env);
 
 /*
  * Class:     sun_lwawt_macosx_CClipboard
+ * Method:    writeObjects
+ * Signature: ([BJ)V
+*/
+JNIEXPORT void JNICALL Java_sun_lwawt_macosx_CClipboard_writeObjects
+(JNIEnv *env, jobject inObject, jbyteArray inBytes, jlong inFormat)
+{
+    if (inBytes == NULL) {
+        return;
+    }
+    
+
+JNF_COCOA_ENTER(env);
+    jint nBytes = (*env)->GetArrayLength(env, inBytes);
+    jbyte *rawBytes = (*env)->GetPrimitiveArrayCritical(env, inBytes, NULL);
+    CHECK_NULL(rawBytes);
+    NSData *bytesAsData = [NSData dataWithBytes:rawBytes length:nBytes];
+    (*env)->ReleasePrimitiveArrayCritical(env, inBytes, rawBytes, JNI_ABORT);
+    NSString *format = formatForIndex(inFormat);
+    NSMutableArray *formatArray = [NSMutableArray arrayWithCapacity:2];
+    const char *bytes = [bytesAsData bytes];
+    char isStart = 1;
+    for (int i = 0; i < [bytesAsData length]; i++) {
+        if ((unsigned char)bytes[i] == 0) {
+            isStart = 1;
+        } else {
+            if (isStart) {
+                isStart = 0;
+                NSString *path = [NSString stringWithUTF8String:(const char *)&bytes[i]];
+                NSURL *fileURL = [NSURL fileURLWithPath:path relativeToURL:nil];
+                [formatArray addObject:fileURL];
+            }
+        }
+    }
+    [ThreadUtilities performOnMainThreadWaiting:YES block:^() {
+        [[NSPasteboard generalPasteboard] writeObjects:formatArray];
+    }];
+JNF_COCOA_EXIT(env);
+}
+
+
+/*
+ * Class:     sun_lwawt_macosx_CClipboard
  * Method:    getClipboardFormats
  * Signature: (J)[J
      */

1div0/JBR

(cherry picked from commit 33db034d49e5af3bb2d01b5302fa7fe2a4d25ef8)
(cherry picked from commit d3ec3d899dad4e190d4a8b14657a7934f12640c0)

src/java.desktop/macosx/native/libawt_lwawt/awt/CClipboard.m
@@ -177,6 +177,48 @@ JNI_COCOA_EXIT(env);
 
 /*
  * Class:     sun_lwawt_macosx_CClipboard
+ * Method:    writeObjects
+ * Signature: ([BJ)V
+*/
+JNIEXPORT void JNICALL Java_sun_lwawt_macosx_CClipboard_writeObjects
+(JNIEnv *env, jobject inObject, jbyteArray inBytes, jlong inFormat)
+{
+    if (inBytes == NULL) {
+        return;
+    }
+    
+
+JNI_COCOA_ENTER(env);
+    jint nBytes = (*env)->GetArrayLength(env, inBytes);
+    jbyte *rawBytes = (*env)->GetPrimitiveArrayCritical(env, inBytes, NULL);
+    CHECK_NULL(rawBytes);
+    NSData *bytesAsData = [NSData dataWithBytes:rawBytes length:nBytes];
+    (*env)->ReleasePrimitiveArrayCritical(env, inBytes, rawBytes, JNI_ABORT);
+    NSString *format = formatForIndex(inFormat);
+    NSMutableArray *formatArray = [NSMutableArray arrayWithCapacity:2];
+    const char *bytes = [bytesAsData bytes];
+    BOOL isStart=YES;
+    for (int i = 0; i < [bytesAsData length]; i++) {
+        if ((unsigned char)bytes[i] == 0) {
+            isStart = YES;
+        } else {
+            if (isStart) {
+                isStart = NO;
+                NSString *path = [NSString stringWithUTF8String:(const char *)&bytes[i]];
+                NSURL *fileURL = [NSURL fileURLWithPath:path relativeToURL:nil];
+                [formatArray addObject:fileURL];
+            }
+        }
+    }
+    [ThreadUtilities performOnMainThreadWaiting:YES block:^() {
+        [[NSPasteboard generalPasteboard] writeObjects:formatArray];
+    }];
+JNI_COCOA_EXIT(env);
+}
+
+
+/*
+ * Class:     sun_lwawt_macosx_CClipboard
  * Method:    getClipboardFormats
  * Signature: (J)[J
      */