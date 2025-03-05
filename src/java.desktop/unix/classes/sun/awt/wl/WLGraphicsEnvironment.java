2lambda123/JetBrainsRuntime

JBR-5645 Provide basic classes for Vulkan rendering pipeline
Implemented shared classes for cross-platform vulkan implementation and some support for wayland toolkit

src/java.desktop/unix/classes/sun/awt/wl/WLGraphicsEnvironment.java
@@ -1,9 +1,35 @@
+/*
+ * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, JetBrains s.r.o.. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
 package sun.awt.wl;
 
 import java.awt.GraphicsDevice;
-import java.lang.ref.WeakReference;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
@@ -16,19 +42,30 @@ import sun.util.logging.PlatformLogger.Level;
 public class WLGraphicsEnvironment extends SunGraphicsEnvironment {
     private static final PlatformLogger log = PlatformLogger.getLogger("sun.awt.wl.WLGraphicsEnvironment");
 
-    private static boolean vkwlAvailable;
-
+    private static boolean vulkanEnabled = false;
+    @SuppressWarnings("removal")
+    private static boolean vulkanRequested =
+            AccessController.doPrivileged(
+                    (PrivilegedAction<Boolean>) () ->
+                            "true".equals(System.getProperty("sun.java2d.vulkan")));
     static {
+        System.loadLibrary("awt");
         SurfaceManagerFactory.setInstance(new UnixSurfaceManagerFactory());
-        vkwlAvailable = initVKWL();
+        if (vulkanRequested) {
+            vulkanEnabled = initVKWL();
+        }
         if (log.isLoggable(Level.INFO)) {
-            log.info("Vulkan rendering available: " + (vkwlAvailable?"YES":"NO"));
+            log.info("Vulkan rendering enabled: " + (vulkanEnabled?"YES":"NO"));
         }
     }
 
     private static native boolean initVKWL();
 
     private WLGraphicsEnvironment() {
+    }
+
+    public static boolean isVulkanEnabled() {
+        return vulkanEnabled;
     }
 
     private static class Holder {
@@ -39,9 +76,6 @@ public class WLGraphicsEnvironment extends SunGraphicsEnvironment {
         return Holder.INSTANCE;
     }
 
-    public static boolean isVKWLAvailable() {
-        return vkwlAvailable;
-    }
 
     @Override
     protected int getNumScreens() {

1div0/JBR

(cherry picked from commit 9ea3d2d0b15aa5470524e123a1bd00de9634d161)

src/java.desktop/unix/classes/sun/awt/wl/WLGraphicsEnvironment.java
@@ -1,7 +1,35 @@
+/*
+ * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, JetBrains s.r.o.. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
 package sun.awt.wl;
 
 import java.awt.EventQueue;
 import java.awt.GraphicsDevice;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -16,19 +44,36 @@ import sun.util.logging.PlatformLogger.Level;
 public class WLGraphicsEnvironment extends SunGraphicsEnvironment {
     private static final PlatformLogger log = PlatformLogger.getLogger("sun.awt.wl.WLGraphicsEnvironment");
 
-    private static boolean vkwlAvailable;
+    private static boolean vulkanEnabled = false;
+    @SuppressWarnings("removal")
+    private static boolean vulkanRequested =
+            AccessController.doPrivileged(
+                    (PrivilegedAction<Boolean>) () ->
+                            "true".equals(System.getProperty("sun.java2d.vulkan")));
+
+    @SuppressWarnings("restricted")
+    private static void loadAwt() {
+        System.loadLibrary("awt");
+    }
 
     static {
+        loadAwt();
         SurfaceManagerFactory.setInstance(new UnixSurfaceManagerFactory());
-        vkwlAvailable = initVKWL();
+        if (vulkanRequested) {
+            vulkanEnabled = initVKWL();
+        }
         if (log.isLoggable(Level.FINE)) {
-            log.fine("Vulkan rendering available: " + (vkwlAvailable?"YES":"NO"));
+            log.fine("Vulkan rendering enabled: " + (vulkanEnabled?"YES":"NO"));
         }
     }
 
     private static native boolean initVKWL();
 
     private WLGraphicsEnvironment() {
+    }
+
+    public static boolean isVulkanEnabled() {
+        return vulkanEnabled;
     }
 
     private static class Holder {
@@ -39,9 +84,6 @@ public class WLGraphicsEnvironment extends SunGraphicsEnvironment {
         return Holder.INSTANCE;
     }
 
-    public static boolean isVKWLAvailable() {
-        return vkwlAvailable;
-    }
 
     @Override
     protected int getNumScreens() {