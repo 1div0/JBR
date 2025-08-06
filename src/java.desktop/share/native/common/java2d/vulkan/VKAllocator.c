JetBrains/JetBrainsRuntime

JBR-7766 Fix VKTexturePool OOM.

src/java.desktop/share/native/common/java2d/vulkan/VKAllocator.c
@@ -191,6 +191,14 @@ void VKAllocator_FindMemoryType(VKMemoryRequirements* requirements,
 static uint32_t VKAllocator_AllocatePage(VKAllocator* alloc, uint32_t memoryType, VkDeviceSize size,
                                          VkImage dedicatedImage, VkBuffer dedicatedBuffer) {
     assert(alloc != NULL);
+    assert(memoryType < VK_MAX_MEMORY_TYPES);
+
+    uint32_t heapIndex = alloc->memoryProperties.memoryTypes[memoryType].heapIndex;
+    VkDeviceSize heapSize = alloc->memoryProperties.memoryHeaps[heapIndex].size;
+    if (size > heapSize) {
+        J2dRlsTraceLn3(J2D_TRACE_ERROR, "VKAllocator_AllocatePage: not enough memory in heap, heapIndex=%d, heapSize=%d, size=%d", heapIndex, heapSize, size);
+        return NO_PAGE_INDEX;
+    }
 
     // Allocate memory.
     VkBool32 dedicated = dedicatedImage != VK_NULL_HANDLE || dedicatedBuffer != VK_NULL_HANDLE;

1div0/JBR

(cherry picked from commit 1a806f4f1b864c9f154aec8867a686066b5d452f)

src/java.desktop/share/native/common/java2d/vulkan/VKAllocator.c
@@ -191,6 +191,14 @@ void VKAllocator_FindMemoryType(VKMemoryRequirements* requirements,
 static uint32_t VKAllocator_AllocatePage(VKAllocator* alloc, uint32_t memoryType, VkDeviceSize size,
                                          VkImage dedicatedImage, VkBuffer dedicatedBuffer) {
     assert(alloc != NULL);
+    assert(memoryType < VK_MAX_MEMORY_TYPES);
+
+    uint32_t heapIndex = alloc->memoryProperties.memoryTypes[memoryType].heapIndex;
+    VkDeviceSize heapSize = alloc->memoryProperties.memoryHeaps[heapIndex].size;
+    if (size > heapSize) {
+        J2dRlsTraceLn(J2D_TRACE_ERROR, "VKAllocator_AllocatePage: not enough memory in heap, heapIndex=%d, heapSize=%d, size=%d", heapIndex, heapSize, size);
+        return NO_PAGE_INDEX;
+    }
 
     // Allocate memory.
     VkBool32 dedicated = dedicatedImage != VK_NULL_HANDLE || dedicatedBuffer != VK_NULL_HANDLE;